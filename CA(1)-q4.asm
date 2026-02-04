de5dc4dca0f0814995f29e740fc6dfccd0047d40
ShreyaBawankar77 <shreyabawankar77@gmail.com>
2026-02-04 21:33:53 +0530

    Refactor assembly code for clarity and formatting

--- CA(1)-q4.asm
+++ CA(1)-q4.asm
@@ -1,6 +1,6 @@
 ORG 0000H            ; Program start address
 
-; -------- Generate first two digits: 86 --------
+; Generate first two digits: 86 
 CLR A                ; A = 00
 
 MOV R0, #080H        ; Upper nibble = 8
@@ -11,7 +11,7 @@ ORL A, R1            ; A = 86H (BCD)
 
 MOV R7, A            ; Store 86 in R7
 
-; -------- Generate last two digits: 84 --------
+;  Generate last two digits: 84 
 CLR A                ; A = 00
 
 MOV R2, #080H        ; Upper nibble = 8
@@ -20,7 +20,8 @@ ORL A, R2            ; A = 80H
 MOV R3, #004H        ; Lower nibble = 4
 ORL A, R3            ; A = 84H (BCD)
 
-; -------- End of program --------
+; End of program 
 SJMP $               ; Stop execution
+
 
 END
